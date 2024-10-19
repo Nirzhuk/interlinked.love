fcec025591634be207efa52fefae3452de8a3b11
Nirzhuk <john.voiden@gmail.com>
2024-10-19 18:26:59 +0200

    feat: tweaking design

--- components/Dashboard/Comments/Comments.tsx
+++ components/Dashboard/Comments/Comments.tsx
@@ -1,5 +1,6 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import type { EventCommentWithUser } from "@/types/comments";
+import { XIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import React from "react";
 
@@ -17,16 +18,21 @@ const Comments = ({ comments, height = "200px" }: CommentsProps) => {
 	}
 
 	return (
-		<ScrollArea className={"w-full  p-4"} style={{ height }}>
+		<ScrollArea className={"w-full p-4"} style={{ height }}>
 			<ul className="space-y-4">
 				{comments.map((comment) => (
 					<li key={comment.id} className={`flex ${comment.userId === user.id ? "justify-end" : "justify-start"}`}>
 						<div
-							className={`max-w-[70%] p-3 rounded-lg ${
+							className={`max-w-[70%] relative p-3 rounded-lg ${
 								comment.userId === user.id ? "bg-blue-100 text-blue-800" : "bg-gray-100 text-gray-800"
 							}`}
 						>
-							<p className="text-sm">{comment.content}</p>
+							{comment.userId === user.id && (
+								<div className="absolute top-2 right-2 -0 w-2 h-2 rounded-full cursor-pointer">
+									<XIcon className="w-2 h-2" />
+								</div>
+							)}
+							<p className="text-sm text-wrap max-w-[200px] overflow-clip break-words">{comment.content}</p>
 							<p className="text-xs text-gray-500 mt-1">
 								{comment.userId === user.id ? "You" : comment.userName} • {new Date(comment.createdAt).toLocaleString()}
 							</p>
