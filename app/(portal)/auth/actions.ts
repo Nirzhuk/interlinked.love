4444481a32ff394bbd264a614a1e880f3677ce84
Nirzhuk <john.voiden@gmail.com>
2024-10-20 20:01:16 +0200

    feat: more debugging

--- app/(portal)/auth/actions.ts
+++ app/(portal)/auth/actions.ts
@@ -61,6 +61,7 @@ export const signUp = validatedAction(signUpSchema, async (data, formData) => {
 	const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
 
 	if (existingUser.length > 0) {
+		console.log("existingUser", existingUser);
 		return { error: "Failed to create user. Please try again." };
 	}
 
@@ -74,8 +75,9 @@ export const signUp = validatedAction(signUpSchema, async (data, formData) => {
 	};
 
 	const [createdUser] = await db.insert(users).values(newUser).returning();
-
+	console.log("createdUser", createdUser);
 	if (!createdUser) {
+		console.log("no createdUser");
 		return { error: "Failed to create user. Please try again." };
 	}
 
